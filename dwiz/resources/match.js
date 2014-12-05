f6060292820c6d3805a81211d633344082908bcf
Nico Rehwaldt <git_nikku@nixis.de>
2014-12-05 17:36:51 +0100

    chore(resources): add sample match

---
+++ dwiz/resources/match.js
@@ -0,0 +1,23 @@
+// POST
+var game = {
+  table: {
+    team1: 'Klo',
+    team2: 'Kaffee'
+  },
+
+  teams: {
+    team1: {
+      offense: 'ROman',
+      defense: 'Nico'
+    },
+    team2: {
+      offense: 'ROman',
+      defense: 'Nico'
+    }
+  },
+
+  played: new Date(),
+
+  score: { team1: 2, team2: 1 },
+  games: [ { team1: 1, team2: 3 }, .... ]
+};
