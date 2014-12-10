f2f7951df73ea085c950654d0e2b217a588fc376
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2014-12-10 18:34:20 +0100

    chore(rest): fix GET /player/{alias}

--- dwiz/app/src/main/java/kickr/core/api/PlayerResource.java
+++ dwiz/app/src/main/java/kickr/core/api/PlayerResource.java
@@ -8,6 +8,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -38,8 +39,9 @@ public class PlayerResource {
   }
   
   @GET
+  @Path("{alias}")
   @UnitOfWork
-  public PlayerData getPlayer(String alias) {
+  public PlayerData getPlayer(@PathParam("alias") String alias) {
     Player player = playerDao.findPlayerByAlias(alias);
     return PlayerData.fromPlayer(player);
   }
