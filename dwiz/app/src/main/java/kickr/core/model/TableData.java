eaa19ff0a7b2fe4160cb9135448b5cf342193969
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2014-12-10 18:52:36 +0100

    chore(rest): add getters/setters for table dto

--- dwiz/app/src/main/java/kickr/core/model/TableData.java
+++ dwiz/app/src/main/java/kickr/core/model/TableData.java
@@ -7,6 +7,7 @@ import kickr.db.entity.FoosballTable;
 
 public class TableData {
   
+  protected Long id;
   protected String name;
   protected String team1Alias;
   protected String team2Alias;
@@ -14,6 +15,7 @@ public class TableData {
   public static TableData fromTable(FoosballTable table) {
     TableData tableData = new TableData();
     
+    tableData.id = table.getId();
     tableData.team1Alias = table.getTeam1Alias();
     tableData.team2Alias = table.getTeam2Alias();
     tableData.name = table.getName();
@@ -29,5 +31,37 @@ public class TableData {
     }
     
     return tablesData;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getTeam1Alias() {
+    return team1Alias;
+  }
+
+  public void setTeam1Alias(String team1Alias) {
+    this.team1Alias = team1Alias;
+  }
+
+  public String getTeam2Alias() {
+    return team2Alias;
+  }
+
+  public void setTeam2Alias(String team2Alias) {
+    this.team2Alias = team2Alias;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
   }
 }
