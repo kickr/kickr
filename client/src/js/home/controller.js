84e0f51a8448295dcecdfb6a9d05d48c229da178
Nico Rehwaldt <git_nikku@nixis.de>
2015-01-13 08:19:25 +0100

    feat(home): display combined (confidence+average) score

--- client/src/js/home/controller.js
+++ client/src/js/home/controller.js
@@ -10,7 +10,7 @@ function HomeController($scope, $alerts, api) {
     $scope.leaderBoard = leaderBoard;
 
     _.forEach(leaderBoard.performance, function(p) {
-      p.average = Math.round((p.score + 0.0) / p.games);
+      p.average = Math.round((0.0 + p.score) / p.games);
       p.confidence = Math.round((1 - 1 / Math.sqrt(p.games * 2)) * 100) / 100;
     });
 
